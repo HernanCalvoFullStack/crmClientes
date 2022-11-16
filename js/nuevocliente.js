92b46c104b6898c1bd9a0781fbd9cadfe03ae16f
Hernán Calvo <hernan.calvo@chediack.com.ar>
2022-11-16 18:35:24 -0300

    Corrección de errores

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -8,6 +8,18 @@
     formulario.addEventListener("submit", validarCliente);
   })
 
+  function conectarDB() {
+    const abrirConexion = window.indexedDB.open("crm", 1);
+
+    abrirConexion.onerror = () => {
+      console.log("Hubo un error");
+    }
+
+    abrirConexion.onsuccess = () => {
+      DB = abrirConexion.result;
+    }
+  }
+
   function validarCliente(e) {
     e.preventDefault();
 
